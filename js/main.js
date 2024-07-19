f472c0f25cb4f33720746a877be8bf304f9e81b7
marwanayman21 <136925068+marwanayman21@users.noreply.github.com>
2024-07-19 19:20:43 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,112 +1,116 @@
-var productsContainer = document.getElementById("product-table-container");
-var warningMessage = document.getElementById("warning-msg");
-var tableBody = document.getElementById("table-body");
+class ProductManager {
+  constructor() {
+    this.productsContainer = document.getElementById("product-table-container");
+    this.warningMessage = document.getElementById("warning-msg");
+    this.tableBody = document.getElementById("table-body");
 
-var products = localStorage.getItem('products') ? JSON.parse(localStorage.getItem('products')) : [];
-var mode = 'add';
-var tempIndex;
+    this.products = localStorage.getItem('products') ? JSON.parse(localStorage.getItem('products')) : [];
+    this.mode = 'add';
+    this.tempIndex = null;
 
-function handleRenderData() {
-  if (products.length !== 0) {
-    productsContainer.classList.remove("d-none");
-    warningMessage.classList.add("d-none");
+    this.productName = document.getElementById("product_name");
+    this.productCat = document.getElementById("product_category");
+    this.productPrice = document.getElementById("product_price");
+    this.productDesc = document.getElementById("product_desc");
+    this.createBtn = document.getElementById("create-btn");
+    this.productForm = document.getElementById("product-form");
+    this.clearBtn = document.getElementById("clear-btn");
 
-    var rows_elements = "";
+    this.productForm.onsubmit = (event) => this.handleFormSubmit(event);
+    this.clearBtn.onclick = () => this.clearData();
 
-    for (var i = 0; i < products.length; i++) {
-      rows_elements += `
-            <tr>
+    this.renderData();
+  }
+
+  renderData() {
+    if (this.products.length !== 0) {
+      this.productsContainer.classList.remove("d-none");
+      this.warningMessage.classList.add("d-none");
+
+      let rowsElements = "";
+
+      for (let i = 0; i < this.products.length; i++) {
+        rowsElements += `
+          <tr>
             <th>${i + 1}</th>
-            <td>${products[i].name}</td>
-            <td>${products[i].cat}</td>
-            <td>${products[i].price}</td>
+            <td>${this.products[i].name}</td>
+            <td>${this.products[i].cat}</td>
+            <td>${this.products[i].price}</td>
+            <td>${this.products[i].desc}</td>
             <td>
-            ${products[i].desc}
-            </td>
-            <td>
-              <button class="btn btn-outline-success" onclick="updateData(${i})">
+              <button class="btn btn-outline-success" onclick="productManager.updateData(${i})">
                 <i class="fa-solid fa-pen-to-square"></i>
               </button>
             </td>
             <td>
-              <button class="btn btn-outline-danger" onclick="deleteData(${i})">
+              <button class="btn btn-outline-danger" onclick="productManager.deleteData(${i})">
                 <i class="fa-solid fa-trash"></i>
               </button>
             </td>
           </tr>
         `;
+      }
+
+      this.tableBody.innerHTML = rowsElements;
+    } else {
+      this.warningMessage.classList.remove("d-none");
+      this.productsContainer.classList.add("d-none");
+    }
+  }
+
+  handleFormSubmit(event) {
+    event.preventDefault();
+
+    if (this.productName.value.trim() === '' || this.productCat.value.trim() === '' || this.productPrice.value.trim() === '' || this.productDesc.value.trim() === '') {
+      alert('Please fill in all fields');
+      return;
     }
 
-    tableBody.innerHTML = rows_elements;
-  } else {
-    warningMessage.classList.remove("d-none");
-    productsContainer.classList.add("d-none");
+    const product = {
+      name: this.productName.value,
+      cat: this.productCat.value,
+      price: this.productPrice.value,
+      desc: this.productDesc.value,
+    };
+
+    if (this.mode === 'add') {
+      this.products.push(product);
+    } else {
+      this.products[this.tempIndex] = product;
+      this.mode = 'add';
+      this.createBtn.innerHTML = 'Add Product';
+    }
+
+    localStorage.setItem('products', JSON.stringify(this.products));
+    this.renderData();
+    this.clearData();
+  }
+
+  clearData() {
+    this.productName.value = '';
+    this.productCat.value = '';
+    this.productPrice.value = '';
+    this.productDesc.value = '';
+  }
+
+  deleteData(index) {
+    this.products.splice(index, 1);
+    localStorage.setItem('products', JSON.stringify(this.products));
+    this.renderData();
+  }
+
+  updateData(index) {
+    this.productName.value = this.products[index].name;
+    this.productCat.value = this.products[index].cat;
+    this.productPrice.value = this.products[index].price;
+    this.productDesc.value = this.products[index].desc;
+
+    this.createBtn.innerHTML = 'Update';
+    this.mode = 'update';
+    this.tempIndex = index;
+    document.getElementById('product-form').scrollIntoView({ behavior: 'smooth' });
   }
 }
 
-handleRenderData();
-
-// Get input elements
-var productName = document.getElementById("product_name");
-var productCat = document.getElementById("product_category");
-var productPrice = document.getElementById("product_price");
-var productDesc = document.getElementById("product_desc");
-var createBtn = document.getElementById("create-btn");
-var productForm = document.getElementById("product-form");
-var clearBtn = document.getElementById("clear-btn");
-
-// Form submission
-productForm.onsubmit = function (event) {
-  event.preventDefault();
-  
-  var product = {
-    name: productName.value,
-    cat: productCat.value,
-    price: productPrice.value,
-    desc: productDesc.value,
-  };
-
-  if (mode === 'add') {
-    products.push(product);
-  } else {
-    products[tempIndex] = product;
-    mode = 'add';
-    createBtn.innerHTML = 'Add Product';
-  }
-
-  localStorage.setItem('products', JSON.stringify(products));
-  handleRenderData();
-  clearData();
-};
-
-// Clear inputs
-clearBtn.onclick = function () {
-  clearData();
-};
-
-function clearData() {
-  productName.value = '';
-  productCat.value = '';
-  productPrice.value = '';
-  productDesc.value = '';
-}
-
-// Delete data
-function deleteData(index) {
-  products.splice(index, 1);
-  localStorage.setItem('products', JSON.stringify(products));
-  handleRenderData();
-}
-
-// Update data
-function updateData(index) {
-  productName.value = products[index].name;
-  productCat.value = products[index].cat;
-  productPrice.value = products[index].price;
-  productDesc.value = products[index].desc;
-  
-  createBtn.innerHTML = 'Update';
-  mode = 'update';
-  tempIndex = index;
-  document.getElementById('product-form').scrollIntoView({ behavior: 'smooth' });
-}
+// Instantiate the ProductManager class
+const productManager = new ProductManager();
